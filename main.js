4a9d4a1fd17474ffb33a2b0c7586e12d981425bb
devinjakhar25 <86140179+devinjakhar25@users.noreply.github.com>
2022-05-30 01:58:22 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -18,5 +18,29 @@ function takesnapshot()
     });
 }
 
-console.log('ml5version:',ml5.version);
+console.log('ml5version:',ml5version);
 classifier=ml5.imageclassifier('ps://teachablemachine.withgoogle.com/models/qlJuP5uhn/model.json',modelLoaded)
+
+  
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+
+function gotResult(error, results) {
+
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
